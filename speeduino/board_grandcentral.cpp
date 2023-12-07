1705fa474a681ad4d6a1d807815a2cb0e9638a82
FoxUnpopuli <68774346+FoxUnpopuli@users.noreply.github.com>
2023-12-07 17:29:04 +0000

    First big edits to board_gc files.

--- speeduino/board_grandcentral.cpp
+++ speeduino/board_grandcentral.cpp
@@ -11,10 +11,10 @@ void initBoard()
 
     /*
     ***********************************************************************************************************
-    * Timers
+    * Schedules
     */
 
-    /*
+      /*
     ***********************************************************************************************************
     * Auxiliaries
     */
@@ -28,9 +28,62 @@ void initBoard()
     ***********************************************************************************************************
     * Schedules
     */
+    // Setup clocks for ALL THE TIMER/COMPARES!
+    MCLK->APBAMASK.reg = MCLK_APBAMASK_TC0 | MCLK_APBAMASK_TC1 ;
+    MCLK->APBBMASK.reg = MCLK_APBBMASK_TC2 | MCLK_APBBMASK_TC3 ;
+    MCLK->APBCMASK.reg = MCLK_APBCMASK_TC4 | MCLK_APBCMASK_TC5 ;
+    MCLK->APBDMASK.reg = MCLK_APBDMASK_TC6 | MCLK_APBDMASK_TC7 ;
+              
+    // Set up the generic clock (GCLK7) bang on 500kHz, 2us tick - for shiggles
+    GCLK->GENCTRL[7].reg = GCLK_GENCTRL_DIV(96) |       // Divide the 48MHz clock source by divisor 96: 48MHz/96 = 500KHz
+                           GCLK_GENCTRL_IDC |           // Set the duty cycle to 50/50 HIGH/LOW
+                           GCLK_GENCTRL_GENEN |         // Enable GCLK7
+                           GCLK_GENCTRL_SRC_DFLL;       // Generate from 48MHz DFLL clock source
+    while (GCLK->SYNCBUSY.bit.GENCTRL7);                // Wait for synchronization
+
+    // Enable all TC perhipheral channels with appropriate GCLK_PCHCTRL_CHEN and...
+    GCLK->PCHCTRL[TC0_GCLK_ID].reg = GCLK_PCHCTRL_CHEN | GCLK_PCHCTRL_GEN_GCLK7;     // Connect generic clock 7 to TC0/1 (9)
+    GCLK->PCHCTRL[TC2_GCLK_ID].reg = GCLK_PCHCTRL_CHEN | GCLK_PCHCTRL_GEN_GCLK7;     // Connect generic clock 7 to TC2/3 (26)
+    GCLK->PCHCTRL[TC4_GCLK_ID].reg = GCLK_PCHCTRL_CHEN | GCLK_PCHCTRL_GEN_GCLK7;     // Connect generic clock 7 to TC4/5 (30)
+    GCLK->PCHCTRL[TC6_GCLK_ID].reg = GCLK_PCHCTRL_CHEN | GCLK_PCHCTRL_GEN_GCLK7;     // Connect generic clock 7 to TC6/7 (39)
+
+    // Set prescaler to 1, set the reset/reload to trigger on the GCLK, set the counter to 16-bit mode...
+    // ... for ALL THE... etc.
+    TC0->COUNT16.CTRLA.reg = TC_CTRLA_PRESCALER_DIV1 | TC_CTRLA_PRESCSYNC_GCLK | TC_CTRLA_MODE_COUNT16;
+    TC1->COUNT16.CTRLA.reg = TC_CTRLA_PRESCALER_DIV1 | TC_CTRLA_PRESCSYNC_GCLK | TC_CTRLA_MODE_COUNT16;
+    TC2->COUNT16.CTRLA.reg = TC_CTRLA_PRESCALER_DIV1 | TC_CTRLA_PRESCSYNC_GCLK | TC_CTRLA_MODE_COUNT16;
+    TC3->COUNT16.CTRLA.reg = TC_CTRLA_PRESCALER_DIV1 | TC_CTRLA_PRESCSYNC_GCLK | TC_CTRLA_MODE_COUNT16;
+    TC4->COUNT16.CTRLA.reg = TC_CTRLA_PRESCALER_DIV1 | TC_CTRLA_PRESCSYNC_GCLK | TC_CTRLA_MODE_COUNT16;
+    TC5->COUNT16.CTRLA.reg = TC_CTRLA_PRESCALER_DIV1 | TC_CTRLA_PRESCSYNC_GCLK | TC_CTRLA_MODE_COUNT16;
+    TC6->COUNT16.CTRLA.reg = TC_CTRLA_PRESCALER_DIV1 | TC_CTRLA_PRESCSYNC_GCLK | TC_CTRLA_MODE_COUNT16;
+    TC7->COUNT16.CTRLA.reg = TC_CTRLA_PRESCALER_DIV1 | TC_CTRLA_PRESCSYNC_GCLK | TC_CTRLA_MODE_COUNT16;
+
+    // Disable all TC interrupts, then enable the MC0 and MC1 interrupts... and the overflow as well just in case
+    TC0->COUNT16.INTENSET.reg = 0; TC0->COUNT16.INTENSET.bit.MC0 = 1; TC0->COUNT16.INTENSET.bit.MC1 = 1; TC0->COUNT16.INTENSET.bit.OVF = 1;
+    TC1->COUNT16.INTENSET.reg = 0; TC1->COUNT16.INTENSET.bit.MC0 = 1; TC1->COUNT16.INTENSET.bit.MC1 = 1; TC1->COUNT16.INTENSET.bit.OVF = 1;
+    TC2->COUNT16.INTENSET.reg = 0; TC2->COUNT16.INTENSET.bit.MC0 = 1; TC2->COUNT16.INTENSET.bit.MC1 = 1; TC2->COUNT16.INTENSET.bit.OVF = 1;
+    TC3->COUNT16.INTENSET.reg = 0; TC3->COUNT16.INTENSET.bit.MC0 = 1; TC3->COUNT16.INTENSET.bit.MC1 = 1; TC3->COUNT16.INTENSET.bit.OVF = 1;
+    TC4->COUNT16.INTENSET.reg = 0; TC4->COUNT16.INTENSET.bit.MC0 = 1; TC4->COUNT16.INTENSET.bit.MC1 = 1; TC4->COUNT16.INTENSET.bit.OVF = 1;
+    TC5->COUNT16.INTENSET.reg = 0; TC5->COUNT16.INTENSET.bit.MC0 = 1; TC5->COUNT16.INTENSET.bit.MC1 = 1; TC5->COUNT16.INTENSET.bit.OVF = 1;
+    TC6->COUNT16.INTENSET.reg = 0; TC6->COUNT16.INTENSET.bit.MC0 = 1; TC6->COUNT16.INTENSET.bit.MC1 = 1; TC6->COUNT16.INTENSET.bit.OVF = 1;
+    TC7->COUNT16.INTENSET.reg = 0; TC7->COUNT16.INTENSET.bit.MC0 = 1; TC7->COUNT16.INTENSET.bit.MC1 = 1; TC7->COUNT16.INTENSET.bit.OVF = 1;
+
+    // Set up all TCs in the Nested Vector Interrupt Controller...
+    // ...all ahead full, warp drive at your command.
+    NVIC_EnableIRQ(TC0_IRQn);
+    NVIC_EnableIRQ(TC1_IRQn);
+    NVIC_EnableIRQ(TC2_IRQn);
+    NVIC_EnableIRQ(TC3_IRQn);
+    NVIC_EnableIRQ(TC4_IRQn);
+    NVIC_EnableIRQ(TC5_IRQn);
+    NVIC_EnableIRQ(TC6_IRQn);
+    NVIC_EnableIRQ(TC7_IRQn);
+  
+
 }
 
 
+// Fun little freeRam routine.
 extern "C" char *sbrk(int i);
 
 uint16_t freeRam() {
@@ -43,5 +96,10 @@ uint16_t freeRam() {
 
 void doSystemReset() { return; }
 void jumpToBootloader() { return; }
+
+// All ISRs 
+
+
+
 
 #endif
--- speeduino/board_grandcentral.h
+++ speeduino/board_grandcentral.h
@@ -28,96 +28,93 @@
 ***********************************************************************************************************
 * Schedules
 * SAMD51 TCs and TCCs, oh what fun.
-* Will initially try Josh's setup for registers, i.e. TCC0->COUNT.reg / TCC0->CC[0].bit.CC
-* Not sure if they'll work or not...
-* Also not sure this can't be done with the simpler TC timers.  Will try later.
-* Fuelling first:
+* Initially tried Josh's setup for registers, i.e. TCC0->COUNT.reg / TCC0->CC[0].bit.CC
+* Didn't finish - don't need the complexity, maybe better keep it simple?  8 TCs with 2 channels each...
+* Probably need to unset all TCs and TCCs from any pins that happen to be attached in the variant file.
+*
+* Fuel first:
 */
   
-  #define FUEL1_COUNTER TCC0->COUNT.reg
-  #define FUEL2_COUNTER TCC0->COUNT.reg
-  #define FUEL3_COUNTER TCC0->COUNT.reg
-  #define FUEL4_COUNTER TCC0->COUNT.reg
-  #define FUEL5_COUNTER TCC0->COUNT.reg
-  #define FUEL6_COUNTER TCC0->COUNT.reg
-  #define FUEL7_COUNTER TCC4->COUNT.reg
-  #define FUEL8_COUNTER TCC4->COUNT.reg
+  #define FUEL1_COUNTER TC0->COUNT16.COUNT.reg
+  #define FUEL2_COUNTER TC0->COUNT16.COUNT.reg
+  #define FUEL3_COUNTER TC1->COUNT16.COUNT.reg
+  #define FUEL4_COUNTER TC1->COUNT16.COUNT.reg
+  #define FUEL5_COUNTER TC4->COUNT16.COUNT.reg
+  #define FUEL6_COUNTER TC4->COUNT16.COUNT.reg
+  #define FUEL7_COUNTER TC5->COUNT16.COUNT.reg
+  #define FUEL8_COUNTER TC5->COUNT16.COUNT.reg
   
-  #define FUEL1_COMPARE TCC0->CC[0].bit.CC
-  #define FUEL2_COMPARE TCC0->CC[1].bit.CC
-  #define FUEL3_COMPARE TCC0->CC[2].bit.CC
-  #define FUEL4_COMPARE TCC0->CC[3].bit.CC
-  #define FUEL5_COMPARE TCC0->CC[4].bit.CC
-  #define FUEL6_COMPARE TCC0->CC[5].bit.CC
-  #define FUEL7_COMPARE TCC4->CC[0].bit.CC
-  #define FUEL8_COMPARE TCC4->CC[1].bit.CC
+  #define FUEL1_COMPARE TC0->COUNT16.CC[0].reg
+  #define FUEL2_COMPARE TC0->COUNT16.CC[1].reg
+  #define FUEL3_COMPARE TC1->COUNT16.CC[0].reg
+  #define FUEL4_COMPARE TC1->COUNT16.CC[1].reg
+  #define FUEL5_COMPARE TC4->COUNT16.CC[0].reg
+  #define FUEL6_COMPARE TC4->COUNT16.CC[1].reg
+  #define FUEL7_COMPARE TC5->COUNT16.CC[0].reg
+  #define FUEL8_COMPARE TC5->COUNT16.CC[1].reg
 
 /*
 * Now Ignitions...
 */
 
-  #define IGN1_COUNTER  TCC1->COUNT.reg
-  #define IGN2_COUNTER  TCC1->COUNT.reg
-  #define IGN3_COUNTER  TCC1->COUNT.reg
-  #define IGN4_COUNTER  TCC1->COUNT.reg
-  #define IGN5_COUNTER  TCC2->COUNT.reg
-  #define IGN6_COUNTER  TCC2->COUNT.reg
-  #define IGN7_COUNTER  TCC3->COUNT.reg
-  #define IGN8_COUNTER  TCC3->COUNT.reg
+  #define IGN1_COUNTER  TC2->COUNT.reg
+  #define IGN2_COUNTER  TC2->COUNT.reg
+  #define IGN3_COUNTER  TC3->COUNT.reg
+  #define IGN4_COUNTER  TC3->COUNT.reg
+  #define IGN5_COUNTER  TC6->COUNT.reg
+  #define IGN6_COUNTER  TC6->COUNT.reg
+  #define IGN7_COUNTER  TC7->COUNT.reg
+  #define IGN8_COUNTER  TC7->COUNT.reg
 
-  #define IGN1_COMPARE  TCC1->CC[0].bit.CC
-  #define IGN2_COMPARE  TCC1->CC[1].bit.CC
-  #define IGN3_COMPARE  TCC1->CC[2].bit.CC
-  #define IGN4_COMPARE  TCC1->CC[3].bit.CC
-  #define IGN5_COMPARE  TCC2->CC[0].bit.CC
-  #define IGN6_COMPARE  TCC2->CC[1].bit.CC
-  #define IGN7_COMPARE  TCC3->CC[0].bit.CC
-  #define IGN8_COMPARE  TCC3->CC[1].bit.CC
+  #define IGN1_COMPARE  TC2->COUNT16.CC[0].reg
+  #define IGN2_COMPARE  TC2->COUNT16.CC[1].reg
+  #define IGN3_COMPARE  TC3->COUNT16.CC[0].reg
+  #define IGN4_COMPARE  TC3->COUNT16.CC[1].reg
+  #define IGN5_COMPARE  TC6->COUNT16.CC[0].reg
+  #define IGN6_COMPARE  TC6->COUNT16.CC[1].reg
+  #define IGN7_COMPARE  TC7->COUNT16.CC[0].reg
+  #define IGN8_COMPARE  TC7->COUNT16.CC[1].reg
 
-  static inline void FUEL1_TIMER_ENABLE(void)  {<macro here>;}
-  static inline void FUEL2_TIMER_ENABLE(void)  {<macro here>;}
+  static inline void FUEL1_TIMER_ENABLE(void)  { TC0->COUNT16.CTRLA.reg |= TC_CTRLA_ENABLE; while (TC0->COUNT16.SYNCBUSY.bit.ENABLE); }
+  static inline void FUEL2_TIMER_ENABLE(void)  { TC0->COUNT16.CTRLA.reg |= TC_CTRLA_ENABLE; while (TC0->COUNT16.SYNCBUSY.bit.ENABLE); }
   static inline void FUEL3_TIMER_ENABLE(void)  {<macro here>;}
   static inline void FUEL4_TIMER_ENABLE(void)  {<macro here>;}
-  //The below are optional, but recommended if there are sufficient timers/compares
   static inline void FUEL5_TIMER_ENABLE(void)  {<macro here>;}
-  static inline void FUEL6_TIMER_ENABLE(void)  {<macro here>;}
+  static inline void FUEL6_TIMER_ENABLE(void)  {<macro here>;}  ///  All these are wrong - I want to enable the MCx, not the whole timer...
   static inline void FUEL7_TIMER_ENABLE(void)  {<macro here>;}
   static inline void FUEL8_TIMER_ENABLE(void)  {<macro here>;}
 
-  static inline void FUEL1_TIMER_DISABLE(void)  { <macro here>;}
-  static inline void FUEL2_TIMER_DISABLE(void)  { <macro here>;}
-  static inline void FUEL3_TIMER_DISABLE(void)  { <macro here>;}
-  static inline void FUEL4_TIMER_DISABLE(void)  { <macro here>;}
-  //The below are optional, but recommended if there are sufficient timers/compares
-  static inline void FUEL5_TIMER_DISABLE(void)  { <macro here>;}
-  static inline void FUEL6_TIMER_DISABLE(void)  { <macro here>;}
-  static inline void FUEL7_TIMER_DISABLE(void)  { <macro here>;}
-  static inline void FUEL8_TIMER_DISABLE(void)  { <macro here>;}
+  static inline void FUEL1_TIMER_DISABLE(void)  { TC0->COUNT16.CTRLA.reg &= ~TC_CTRLA_ENABLE; while (TC0->COUNT16.SYNCBUSY.bit.ENABLE); }
+  static inline void FUEL2_TIMER_DISABLE(void)  { TC0->COUNT16.CTRLA.reg &= ~TC_CTRLA_ENABLE; while (TC0->COUNT16.SYNCBUSY.bit.ENABLE); }
+  static inline void FUEL3_TIMER_DISABLE(void)  { TC1->COUNT16.CTRLA.reg &= ~TC_CTRLA_ENABLE; while (TC1->COUNT16.SYNCBUSY.bit.ENABLE); }
+  static inline void FUEL4_TIMER_DISABLE(void)  { TC1->COUNT16.CTRLA.reg &= ~TC_CTRLA_ENABLE; while (TC1->COUNT16.SYNCBUSY.bit.ENABLE); }
+  static inline void FUEL5_TIMER_DISABLE(void)  { TC4->COUNT16.CTRLA.reg &= ~TC_CTRLA_ENABLE; while (TC4->COUNT16.SYNCBUSY.bit.ENABLE); }
+  static inline void FUEL6_TIMER_DISABLE(void)  { TC4->COUNT16.CTRLA.reg &= ~TC_CTRLA_ENABLE; while (TC4->COUNT16.SYNCBUSY.bit.ENABLE); }
+  static inline void FUEL7_TIMER_DISABLE(void)  { TC5->COUNT16.CTRLA.reg &= ~TC_CTRLA_ENABLE; while (TC5->COUNT16.SYNCBUSY.bit.ENABLE); }
+  static inline void FUEL8_TIMER_DISABLE(void)  { TC5->COUNT16.CTRLA.reg &= ~TC_CTRLA_ENABLE; while (TC5->COUNT16.SYNCBUSY.bit.ENABLE); }
 
     static inline void IGN1_TIMER_ENABLE(void)  {<macro here>;}
     static inline void IGN2_TIMER_ENABLE(void)  {<macro here>;}
     static inline void IGN3_TIMER_ENABLE(void)  {<macro here>;}
     static inline void IGN4_TIMER_ENABLE(void)  {<macro here>;}
-  //The below are optional, but recommended if there are sufficient timers/compares
     static inline void IGN5_TIMER_ENABLE(void)  {<macro here>;}
     static inline void IGN6_TIMER_ENABLE(void)  {<macro here>;}
     static inline void IGN7_TIMER_ENABLE(void)  {<macro here>;}
     static inline void IGN8_TIMER_ENABLE(void)  {<macro here>;}
 
-    static inline void IGN1_TIMER_DISABLE(void)  {<macro here>;}
-    static inline void IGN2_TIMER_DISABLE(void)  {<macro here>;}
-    static inline void IGN3_TIMER_DISABLE(void)  {<macro here>;}
-    static inline void IGN4_TIMER_DISABLE(void)  {<macro here>;}
-  //The below are optional, but recommended if there are suffici;}ent timers/compares
-    static inline void IGN5_TIMER_DISABLE(void)  {<macro here>;}
-    static inline void IGN6_TIMER_DISABLE(void)  {<macro here>;}
-    static inline void IGN7_TIMER_DISABLE(void)  {<macro here>;}
-    static inline void IGN8_TIMER_DISABLE(void)  {<macro here>;}
+    static inline void IGN1_TIMER_DISABLE(void)  { TC2->COUNT16.CTRLA.reg &= ~TC_CTRLA_ENABLE; while (TC2->COUNT16.SYNCBUSY.bit.ENABLE); }
+    static inline void IGN2_TIMER_DISABLE(void)  { TC2->COUNT16.CTRLA.reg &= ~TC_CTRLA_ENABLE; while (TC2->COUNT16.SYNCBUSY.bit.ENABLE); }
+    static inline void IGN3_TIMER_DISABLE(void)  { TC3->COUNT16.CTRLA.reg &= ~TC_CTRLA_ENABLE; while (TC3->COUNT16.SYNCBUSY.bit.ENABLE); }
+    static inline void IGN4_TIMER_DISABLE(void)  { TC3->COUNT16.CTRLA.reg &= ~TC_CTRLA_ENABLE; while (TC3->COUNT16.SYNCBUSY.bit.ENABLE); }
+    static inline void IGN5_TIMER_DISABLE(void)  { TC6->COUNT16.CTRLA.reg &= ~TC_CTRLA_ENABLE; while (TC6->COUNT16.SYNCBUSY.bit.ENABLE); }
+    static inline void IGN6_TIMER_DISABLE(void)  { TC6->COUNT16.CTRLA.reg &= ~TC_CTRLA_ENABLE; while (TC6->COUNT16.SYNCBUSY.bit.ENABLE); }
+    static inline void IGN7_TIMER_DISABLE(void)  { TC7->COUNT16.CTRLA.reg &= ~TC_CTRLA_ENABLE; while (TC7->COUNT16.SYNCBUSY.bit.ENABLE); }
+    static inline void IGN8_TIMER_DISABLE(void)  { TC7->COUNT16.CTRLA.reg &= ~TC_CTRLA_ENABLE; while (TC7->COUNT16.SYNCBUSY.bit.ENABLE); }
 
   
-  #define MAX_TIMER_PERIOD 139808 //This is the maximum time, in uS, that the compare channels can run before overflowing. It is typically 65535 * <how long each tick represents>
-  #define uS_TO_TIMER_COMPARE(uS) ((uS * 15) >> 5) //Converts a given number of uS into the required number of timer ticks until that time has passed.
-
+  // Yet another tick-period... 2us
+  #define MAX_TIMER_PERIOD 131070 //The longest period of time (in uS) that the timer can permit (In this case it is 65535 * 2, as each timer tick is 2uS)  (Why not 65536 * 2?)
+  #define uS_TO_TIMER_COMPARE(uS) ((uS) >> 1) //Converts a given number of uS into the required number of timer ticks until that time has passed
 /*
 ***********************************************************************************************************
 * Auxiliaries
